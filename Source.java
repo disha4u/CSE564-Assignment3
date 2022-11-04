9d5c0a5ca2ecaae7b00e1c4365815728a96c30e5
disha4u <disha.97.agarwal@gmail.com>
2022-11-04 15:52:18 -0700

    datagen

--- Source.java
+++ Source.java
@@ -1,5 +1,16 @@
-
+import java.util.Random;
 public class Source extends Observable {
-	protected Top top;
-
+	private Double x;
+	   public void GenerateData(){
+		      Random rand = new Random(); //instance of random class
+		      double upperbound = 25.00;
+		        //generate random values from 0-24
+		      while(true){
+		      x=rand.nextDouble(upperbound);
+		      Notify();
+		      }
+	   } 
+       public Double getpoint() {
+    	   return x;
+       }
 }
