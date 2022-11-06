abc99e5208ed01e9d3643df6d6df62aa74a7f1da
disha4u <disha.97.agarwal@gmail.com>
2022-11-05 18:11:06 -0700

    adding q

--- Source.java
+++ Source.java
@@ -1,19 +1,31 @@
+import java.util.*;
+import java.util.Queue;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Source extends Observable {
-	private Double x;
+		
+	Double[] y = {1, 2, 3, 4,5,6,7,8,9,10};
+	ArrayList<Double> q=new ArrayList<Double>(Collections.nCopies(10, 0.0));
+	Double cury;
+	
 	   public void GenerateData(){
 		      Random rand = new Random(); //instance of random class
-		      double upperbound = 25.00;
+		      double upperbound = 9;
 		        //generate random values from 0-24
+		      i=0;
 		      while(true){
-		      x=rand.nextDouble(upperbound);
+		      q.set(i%10,100*rand.nextDouble());
+		      cury=y[rand.nextInt(upperbound)];
+		      i++;
 		      Notify();
 		      TimeUnit.SECONDS.sleep(20);
 		      }
 	   } 
-       public Double getpoint() {
-    	   return x;
+       public Double getData() {
+    	   return q;
+       }
+       public Double getY() {
+    	   return cury;
        }
 }
